6b9942cd865b405f536c36deb09e0749a742bf81
archa347 <daniel.r.gallegos@gmail.com>
2014-07-29 15:23:19 -0700

    Update Post.java
    Works when always moving in +x or +y direction.  TODO: Modify to handle necessary backtracking

--- Post.java
+++ Post.java
@@ -17,15 +17,15 @@ public class Post {
 		
 		TestCase[] cases=null;
 		
-		Pattern testDefinition = Pattern.compile("\\d+ \\d+ \\d+");
-		Pattern intersection = Pattern.compile("\\d+ \\d+");
+		Pattern testDefinition = Pattern.compile("(\\d+) (\\d+) (\\d+)");
+		Pattern intersection = Pattern.compile("(\\d+) (\\d+)");
 		
 		
 		try{
 			BufferedReader c = new BufferedReader(new InputStreamReader(System.in));
 			//Console c = System.console();
 			int testCases = Integer.parseInt(c.readLine());
-			cases = new TestCase[testCases+1];
+			cases = new TestCase[testCases];
 			
 			for (int i=0; i < testCases; i++) {
 				c.readLine();
@@ -36,7 +36,7 @@ public class Post {
 					newCase.w = Integer.parseInt(testMatcher.group(1));
 					newCase.h = Integer.parseInt(testMatcher.group(2));
 					int intersections = Integer.parseInt(testMatcher.group(3));
-					newCase.intersections = new boolean[newCase.w][newCase.h];
+					newCase.intersections = new boolean[newCase.w+1][newCase.h+1];
 					for (int j = 0; j < intersections; j++) {
 						Matcher matcher = intersection.matcher(c.readLine());
 						if (matcher.matches()) {
@@ -54,22 +54,39 @@ public class Post {
 		
 		for (int caze = 0; caze < cases.length; caze++) {
 			TestCase cur = cases[caze];
-			cur.result = new int[cur.w][cur.h];
+			cur.result = new int[cur.w+1][cur.h+1];
 			
-			for (int x = 0; x < cur.w; x++) {
+			for (int i = 0; i <= cur.w; i++) {
+				if (cur.intersections[i][0]) {
+					cur.result[i][0]= 0;
+					break;
+				}
+				cur.result[i][0] = 1;
+			}
+			
+
+			for (int i = 0; i <= cur.h; i++) {
+				if (cur.intersections[0][i]) {
+					cur.result[0][i]= 0;
+					break;
+				}
+				cur.result[0][i] = 1;
+			}
+			
+			for (int x = 1; x <= cur.w; x++) {
 				
-				for (int y=0; y < cur.h; y++) {
+				for (int y=1; y <= cur.h; y++) {
 					if (cur.intersections[x][y]) {
-						cur.result[x][y]= -1;
-						break;
+						cur.result[x][y]= 0;
+						continue;
 					}
 					
 					int dx,dy;
 					if (x-1 < 0) dx = 0;
-					else dx = 1 + cur.result[x-1][y];
+					else dx = cur.result[x-1][y];
 					
 					if (y-1 < 0) dy = 0;
-					else dy = 1 + cur.result[x][y-1];
+					else dy = cur.result[x][y-1];
 						
 					cur.result[x][y] = dx + dy;
 				}
